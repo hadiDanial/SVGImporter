c9af9a64a9b01020429f7ec9e8ce476b6c828038
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-05 22:54:34 +0200

    Parse tag attributes

--- SVGImporter/SVG/Attribute.cs
+++ SVGImporter/SVG/Attribute.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SVGImporter.SVG
 {
@@ -10,6 +13,7 @@ namespace SVGImporter.SVG
     {
         public string attributeName;
         public string attributeValue;
+        private const string PATTERN = "[ ]([A-Z]|[a-z]|[0-9])+=\"([A-Z]|[a-z]|[0-9]|[ ])+\"";
 
         public Attribute(string attributeName, string attributeValue)
         {
@@ -20,6 +24,32 @@ namespace SVGImporter.SVG
         public override string ToString()
         {
             return $"{attributeName}=\"{attributeValue}\"";
+        }
+        public static List<Attribute> SVGToAttributes(string tag)
+        {
+            List<Attribute> attributes = new List<Attribute>();
+            Regex regex = new Regex(PATTERN);
+            MatchCollection matches = regex.Matches(tag);
+            foreach (Match match in matches)
+            {
+                Attribute attr = new Attribute();
+                string[] split = match.Value.Split('=');
+                if (split == null || split.Length != 2) continue;
+                attr.attributeName = split[0].Trim();
+                attr.attributeValue = split[1].Replace('\"',' ').Trim();
+                attributes.Add(attr);
+            }
+            return attributes;
+        }
+        public static string AttributesToSVG(List<Attribute> attributes)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (Attribute attribute in attributes)
+            {
+                stringBuilder.Append(attribute);
+                stringBuilder.Append(' ');
+            }
+            return stringBuilder.ToString();
         }
     }
 }
