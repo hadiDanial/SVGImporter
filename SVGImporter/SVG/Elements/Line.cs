8b29e674820e754cac242771e32c7975b6d21475
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-05 22:55:48 +0200

    Parse Line

--- SVGImporter/SVG/Elements/Line.cs
+++ SVGImporter/SVG/Elements/Line.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SVGImporter.SVG.Elements
 {
-    internal class Line:Element
+    internal class Line : Element
     {
         private Vector2 point1, point2;
 
@@ -20,6 +20,29 @@ namespace SVGImporter.SVG.Elements
         public override string ElementToSVGTag()
         {
             return $"<{GetElementName()} x1=\"{point1.x}\" y1=\"{point1.y}\" x2=\"{point2.x}\" y2=\"{point2.y}\" {AttributesToSVG()} />";
+        }
+
+        public static new Line GetElement(string tagText)
+        {
+            List<Attribute> attributes = Attribute.SVGToAttributes(tagText);
+            float x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+            float val;
+            foreach (Attribute attribute in attributes)
+            {
+                if (attribute.attributeName.Equals("x1") && float.TryParse(attribute.attributeValue, out val))
+                    x1 = val;
+                if (attribute.attributeName.Equals("y1") && float.TryParse(attribute.attributeValue, out val))
+                    y1 = val;
+                if (attribute.attributeName.Equals("x2") && float.TryParse(attribute.attributeValue, out val))
+                    x2 = val;
+                if (attribute.attributeName.Equals("y2") && float.TryParse(attribute.attributeValue, out val))
+                    y2 = val;
+            }
+            Vector2 p1 = new Vector2(x1, y1);
+            Vector2 p2 = new Vector2(x2, y2);
+
+            Line line = new Line(tagText, null, p1, p2, attributes);
+            return line;
         }
 
         public override string GetElementName()
