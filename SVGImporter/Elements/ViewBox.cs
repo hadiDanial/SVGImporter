1adfe5f99f824e1be49b7819ee3d4e6a0f8f80b6
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-06 21:53:07 +0200

    Update ViewBox.cs

--- SVGImporter/Elements/ViewBox.cs
+++ SVGImporter/Elements/ViewBox.cs
@@ -11,12 +11,16 @@ namespace SVGImporter.Elements
     {
         private Vector2 size;
         private Vector2 origin;
-
+        private bool defined = false;
         public ViewBox(Vector2 size, Vector2 origin)
         {
             this.size = size;
             this.origin = origin;
+            defined = true;
         }
+        public ViewBox() { }
+
+        public bool Defined { get => defined; }
 
         //public Vector2 GetPoint(Vector2 point)
         //{
