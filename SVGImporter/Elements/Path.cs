e1e1ff3abd9214e9a4a1b1c5338e443cdd1601e1
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 22:09:53 +0200

    Added regex to get commands in path data and generate PathCommands

--- SVGImporter/Elements/Path.cs
+++ SVGImporter/Elements/Path.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using SVGImporter.Elements;
 using SVGImporter.Elements.PathUtility;
@@ -12,9 +13,15 @@ namespace SVGImporter.Elements
     {
         private List<PathCommand> pathCommands;
         private string pathData = string.Empty;
+        private bool isClosedPath;
+        private const string COMMAND_REGEX_PATTERN = "(([mzlhvcsqta]|[MZLHVCSQTA])( *)(\\d+\\.\\d+|\\d+)(([ |,|\\-])*(\\d+\\.\\d+|\\d+))*|z)";
+
+        public bool IsClosedPath { get => isClosedPath; set => isClosedPath = value; }
+
         internal Path(List<TagAttribute> attributes) : base(attributes)
         {
             pathCommands = new List<PathCommand>();
+            IsClosedPath = false;
             foreach (var attribute in attributes)
             {
                 if (attribute.attributeName.Equals("d"))
@@ -27,6 +34,17 @@ namespace SVGImporter.Elements
         {
             if(string.IsNullOrEmpty(pathData))
                 return;
+            Regex regex = new Regex(COMMAND_REGEX_PATTERN);
+            MatchCollection matches = regex.Matches(pathData);
+            foreach (Match match in matches)
+            {
+                PathCommand command = PathCommand.CreateCommand(match.Value);
+                if (command == null) continue; // TODO: Remove, this is only here because nothing is implemented yet
+                if (command.Equals(ClosePathCommand.INSTANCE))
+                    IsClosedPath = true;
+                else
+                    pathCommands.Add(command);
+            }
         }
 
         public override string ElementToSVGTag()
