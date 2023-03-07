fddf4762cacca789351475d433f3cd0a04cf627b
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 22:11:12 +0200

    Create MoveCommand.cs

---
+++ SVGImporter/Elements/PathUtility/MoveCommand.cs
@@ -0,0 +1,18 @@
+﻿using SVGImporter.Utility;
+
+namespace SVGImporter.Elements.PathUtility
+{
+    internal class MoveCommand : PathCommand
+    {
+        private Vector2 point;
+        public Vector2 Point { get => point; set => point = value; }
+
+        public MoveCommand(string data, bool isAbsolute) : base(data, isAbsolute)
+        {
+            if (values == null || values.Count != 2)
+                throw new InvalidDataException($"Move command must have 2 values!");
+            point = new Vector2(values[0], values[1]);
+        }
+
+    }
+}
