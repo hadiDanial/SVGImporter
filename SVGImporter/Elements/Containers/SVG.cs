3898c0fbfcb56eed1477d27f4d4189277717586c
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 11:50:26 +0200

    Update SVG.cs

--- SVGImporter/Elements/Containers/SVG.cs
+++ SVGImporter/Elements/Containers/SVG.cs
@@ -9,12 +9,35 @@ namespace SVGImporter.Elements.Containers
         private Vector2 size;
         private ViewBox viewBox;
 
-        protected SVG(string tagText, List<TagAttribute> attributes, Vector2 size, ViewBox viewBox) : base(tagText, attributes)
+        internal SVG(List<TagAttribute> attributes) : base(attributes)
         {
-            this.size = size;
+            ViewBox viewBox = new ViewBox();
+            float width = 0, height = 0, vWidth = 0, vHeight = 0, minX = 0, minY = 0, val;
+            foreach (TagAttribute attribute in attributes)
+            {
+                if (attribute.attributeName.Equals("width") && float.TryParse(attribute.attributeValue, out val))
+                    width = val;
+                if (attribute.attributeName.Equals("height") && float.TryParse(attribute.attributeValue, out val))
+                    height = val;
+                if (attribute.attributeName.Equals("viewBox"))
+                {
+                    string[] values = attribute.attributeValue.Split(' ');
+                    if (values == null || values.Length != 4)
+                        throw new InvalidDataException($"Invalid ViewBox values for {GetElementName(TagType.SVG)}");
+                    if (float.TryParse(attribute.attributeValue, out val))
+                        minX = val;
+                    if (float.TryParse(attribute.attributeValue, out val))
+                        minY = val;
+                    if (float.TryParse(attribute.attributeValue, out val))
+                        vWidth = val;
+                    if (float.TryParse(attribute.attributeValue, out val))
+                        vHeight = val;
+                    viewBox = new ViewBox(new Vector2(vWidth, vHeight), new Vector2(minX, minY));
+                }
+            }
             this.viewBox = viewBox;
+            this.size = new Vector2(width, height);
         }
-
 
         public static new SVG GetElement(string tagText)
         {
@@ -47,7 +70,7 @@ namespace SVGImporter.Elements.Containers
                 }
 
             }
-            SVG svg = new SVG(tagText, attributes, new Vector2(width, height), viewBox);
+            SVG svg = new SVG(attributes);//, new Vector2(width, height), viewBox);
             svg.value = value;
             svg.children = Element.GetElements(content);
             //Console.WriteLine("Content:\n" + content);
