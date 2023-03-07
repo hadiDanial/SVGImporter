a069c91aa34afd3516d2aef751bd9db630900d5e
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 22:11:23 +0200

    Create ClosePathCommand.cs

---
+++ SVGImporter/Elements/PathUtility/ClosePathCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace SVGImporter.Elements.PathUtility
+{
+    internal class ClosePathCommand : PathCommand
+    {
+        private ClosePathCommand(string data, bool isAbsolute) : base(data, isAbsolute)
+        {
+        }
+
+        public static PathCommand INSTANCE
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ClosePathCommand(string.Empty, false);
+                return instance;
+            }
+            set
+            {
+                INSTANCE = value;
+            }
+        }
+
+        private static ClosePathCommand instance = null;
+    }
+}
