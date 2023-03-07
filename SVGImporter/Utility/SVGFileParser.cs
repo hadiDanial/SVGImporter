1005fdc25d4469d26253c528342fc27eb96a8495
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 12:17:03 +0200

    Parse SVG using XElement

--- SVGImporter/Utility/SVGFileParser.cs
+++ SVGImporter/Utility/SVGFileParser.cs
@@ -1,7 +1,9 @@
 ﻿using SVGImporter.Elements;
 using SVGImporter.Elements.Containers;
+using System;
 using System.ComponentModel;
 using System.IO;
+using System.Xml.Linq;
 
 namespace SVGImporter.Utility
 {
@@ -32,12 +34,31 @@ namespace SVGImporter.Utility
             {
                 throw new InvalidDataException("No <svg> tag found!");
             }
-            svgText = svgText.Substring(start);
-            SVG svgElement = (SVG)Element.GetElement(svgText);
+            var doc = XDocument.Parse(svgText);
+            XElement root = doc.Root;
+            XNamespace xNamespace = root.GetDefaultNamespace();
+            Element svg = ParseDocumentRecusrively(root, xNamespace);
+            SaveFile(svg.ElementToSVGTag(), path);
+            //Console.WriteLine(svg.ElementToSVGTag());
+        }
 
-            Console.WriteLine("Element To SVG:\n");
-            Console.WriteLine(svgElement.ElementToSVGTag());
-            SaveFile(svgElement.ElementToSVGTag(), path);
+        private static Element ParseDocumentRecusrively(XElement root, XNamespace xNamespace)
+        {
+            TagType type = Element.GetTypeByName(root.Name.LocalName);
+            List<TagAttribute> attributes = TagAttribute.FromXElementAttributes(root.Attributes());
+            Element createdElement = Element.CreateElement(type, root.Value, attributes);
+            if (root.HasElements)
+            {
+                List<Element> elementsList = new List<Element>();
+                var elements = root.Elements();
+                foreach (var element in elements)
+                {
+                    elementsList.Add(ParseDocumentRecusrively(element, xNamespace));
+                }
+                ((ParentElement)createdElement).SetChildren(elementsList);
+            }
+
+            return createdElement;
         }
 
         public static void SaveFile(string generatedSVG, string path, bool localPath = true)
