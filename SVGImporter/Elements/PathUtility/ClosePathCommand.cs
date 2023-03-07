d690fc2548c8093b429bc8a030a2a7fe30a3be3d
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-08 00:01:12 +0200

    Update ClosePathCommand.cs

--- SVGImporter/Elements/PathUtility/ClosePathCommand.cs
+++ SVGImporter/Elements/PathUtility/ClosePathCommand.cs
@@ -2,24 +2,9 @@
 {
     internal class ClosePathCommand : PathCommand
     {
-        private ClosePathCommand(string data, bool isAbsolute) : base(data, isAbsolute)
+        internal ClosePathCommand(string data, bool isAbsolute) : base(data, isAbsolute)
         {
         }
 
-        public static PathCommand INSTANCE
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new ClosePathCommand(string.Empty, false);
-                return instance;
-            }
-            set
-            {
-                INSTANCE = value;
-            }
-        }
-
-        private static ClosePathCommand instance = null;
     }
 }
