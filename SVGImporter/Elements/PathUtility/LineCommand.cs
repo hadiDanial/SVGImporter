839f772bedeaf78780eb3394aca795c129407a90
hadiDanial <hadiDanial@users.noreply.github.com>
2023-03-07 22:11:17 +0200

    Create LineCommand.cs

---
+++ SVGImporter/Elements/PathUtility/LineCommand.cs
@@ -0,0 +1,17 @@
+﻿using SVGImporter.Utility;
+
+namespace SVGImporter.Elements.PathUtility
+{
+    internal class LineCommand : PathCommand
+    {
+        private Vector2 point;
+        public Vector2 Point { get => point; set => point = value; }
+
+        public LineCommand(string data, bool isAbsolute) : base(data, isAbsolute)
+        {
+            if (values == null || values.Count != 2)
+                throw new InvalidDataException($"Line command must have 2 values!");
+            point = new Vector2(values[0], values[1]);
+        }
+    }
+}
