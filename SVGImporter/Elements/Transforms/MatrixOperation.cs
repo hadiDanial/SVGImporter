64d1e6eb1e294d97bdf0d1c9f801229b20586c17
hadiDanial <hadiDanial@users.noreply.github.com>
2023-04-06 04:03:35 +0300

    Scaled MatrixOperation by scaleFactor

--- SVGImporter/Elements/Transforms/MatrixOperation.cs
+++ SVGImporter/Elements/Transforms/MatrixOperation.cs
@@ -4,6 +4,7 @@ namespace SVGImporter.Elements.Transforms
 {
     public class MatrixOperation : TransformOperation
     {
+        private readonly float a, b, c, d, e, f;
         public Matrix4x4 Matrix { get; }
 
         public MatrixOperation(Matrix4x4 matrix)
@@ -13,11 +14,22 @@ namespace SVGImporter.Elements.Transforms
 
         public MatrixOperation(float a, float b, float c, float d, float e, float f)
         {
-            Matrix = new Matrix4x4(
-                new Vector4(a, b, 0, 0),
-                new Vector4(c, d, 0, 0),
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.e = e;
+            this.f = f;
+            Matrix = CreateMatrix(1f);
+        }
+
+        private Matrix4x4 CreateMatrix(float scaleFactor)
+        {
+            return new Matrix4x4(
+                new Vector4(a * scaleFactor, b * scaleFactor, 0, 0),
+                new Vector4(c * scaleFactor, d * scaleFactor, 0, 0),
                 new Vector4(0, 0, 1, 0),
-                new Vector4(e, f, 0, 1)
+                new Vector4(e * scaleFactor, f * scaleFactor, 0, 1)
             );
         }
 
@@ -29,8 +41,8 @@ namespace SVGImporter.Elements.Transforms
 
         public override void ApplyTo(Transform transform, float scaleFactor = 1f)
         {
-            Matrix4x4 currentMatrix = Matrix4x4.TRS(transform.position * scaleFactor, transform.rotation, transform.localScale * scaleFactor);
-            Matrix4x4 newMatrix = Matrix * currentMatrix;
+            Matrix4x4 currentMatrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
+            Matrix4x4 newMatrix = CreateMatrix(scaleFactor) * currentMatrix;
             transform.position = newMatrix.GetColumn(3);
             transform.rotation = Quaternion.LookRotation(newMatrix.GetColumn(2), newMatrix.GetColumn(1));
             transform.localScale = newMatrix.lossyScale;
